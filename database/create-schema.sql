6ca63e9fd34e85584b6a8fff8bb4c8479de39359
AGryshchenko <andrewgryshchenko@gmail.com>
2016-09-06 10:57:06 +0300

    Enabling in-memory DB step 1

---
+++ database/create-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE  (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        first_name varchar not null,
+        second_name varchar not null,
+        phone varchar null
+        );
---
+++ webshop-webapp/src/main/java/org/midnightcoding/config/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package org.midnightcoding.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("create-schema.sql")
+                .addScripts("initial-data.sql")
+                .build();
+    }
+}
